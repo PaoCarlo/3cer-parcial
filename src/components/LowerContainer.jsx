311803c8a7e271313a86ba9a00d0b658567d8131
Paola Carlo <csusi3988@gmail.com>
2023-06-23 12:16:39 -0400

    last comit

---
+++ src/components/LowerContainer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/LowerContainer.css";
+
+function LowerContainer() {
+  return (
+    <div className="lower">
+      <h2>Estandar Cactus</h2>
+      <h3>Succulent</h3>
+    </div>
+  );
+}
+
+export { LowerContainer };
+
